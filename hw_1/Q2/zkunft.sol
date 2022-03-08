e72fae37b43d2836fabe2f9d8ea1f5f959b1ffdc
WudingRoad1145 <41933186+WudingRoad1145@users.noreply.github.com>
2022-03-08 00:11:51 -0500

    Update zkunft.sol
    updated minor error

--- hw_1/Q2/zkunft.sol
+++ hw_1/Q2/zkunft.sol
@@ -1,300 +1,56 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-library Base64 {
-    string internal constant TABLE_ENCODE =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-    bytes internal constant TABLE_DECODE =
-        hex"0000000000000000000000000000000000000000000000000000000000000000"
-        hex"00000000000000000000003e0000003f3435363738393a3b3c3d000000000000"
-        hex"00000102030405060708090a0b0c0d0e0f101112131415161718190000000000"
-        hex"001a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132330000000000";
+contract myNFT is ERC721URIStorage{
+    using Strings for uint256;
+    uint256 constant public totalSupply = 100;
+    uint256 private _tokenCounter;
 
-    function encode(bytes memory data) internal pure returns (string memory) {
-        if (data.length == 0) return "";
+    // nodes = leaves * 2 - 1
+    bytes32[totalSupply * 2 - 1] private _merkleTreeNodes; 
 
-        // load the table into memory
-        string memory table = TABLE_ENCODE;
+    constructor() ERC721("Dwen Dwen", "BDD") {}
 
-        // multiply by 4/3 rounded up
-        uint256 encodedLen = 4 * ((data.length + 2) / 3);
-
-        // add some extra buffer at the end required for the writing
-        string memory result = new string(encodedLen + 32);
-
-        assembly {
-            // set the actual output length
-            mstore(result, encodedLen)
-
-            // prepare the lookup table
-            let tablePtr := add(table, 1)
-
-            // input ptr
-            let dataPtr := data
-            let endPtr := add(dataPtr, mload(data))
-
-            // result ptr, jump over length
-            let resultPtr := add(result, 32)
-
-            // run over the input, 3 bytes at a time
-            for {
-
-            } lt(dataPtr, endPtr) {
-
-            } {
-                // read 3 bytes
-                dataPtr := add(dataPtr, 3)
-                let input := mload(dataPtr)
-
-                // write 4 characters
-                mstore8(
-                    resultPtr,
-                    mload(add(tablePtr, and(shr(18, input), 0x3F)))
-                )
-                resultPtr := add(resultPtr, 1)
-                mstore8(
-                    resultPtr,
-                    mload(add(tablePtr, and(shr(12, input), 0x3F)))
-                )
-                resultPtr := add(resultPtr, 1)
-                mstore8(
-                    resultPtr,
-                    mload(add(tablePtr, and(shr(6, input), 0x3F)))
-                )
-                resultPtr := add(resultPtr, 1)
-                mstore8(resultPtr, mload(add(tablePtr, and(input, 0x3F))))
-                resultPtr := add(resultPtr, 1)
-            }
-
-            // padding with '='
-            switch mod(mload(data), 3)
-            case 1 {
-                mstore(sub(resultPtr, 2), shl(240, 0x3d3d))
-            }
-            case 2 {
-                mstore(sub(resultPtr, 1), shl(248, 0x3d))
-            }
+    function mintToAddress(address _to) public {
+        require(_tokenCounter < totalSupply, "Mint is over!");
+        unchecked {
+            _tokenCounter++;
         }
 
-        return result;
-    }
+        _mint(_to, _tokenCounter);
+        string memory _metadata = string(abi.encodePacked(
+            "{",
+                "\"name\": \"DwenDwen #", _tokenCounter.toString(), "\",",
+                "\"description\": \"Edition", _tokenCounter.toString(), " out of ", totalSupply.toString(), '"',
+            "}"
+            ));
+        _setTokenURI(_tokenCounter, _metadata);
+        
+        // merkle tree
+        uint256 _nodeIndex = _tokenCounter - 1;
 
-    function decode(string memory _data) internal pure returns (bytes memory) {
-        bytes memory data = bytes(_data);
+        _merkleTreeNodes[_nodeIndex] = keccak256(abi.encodePacked(msg.sender, _to, _tokenCounter, _metadata));
 
-        if (data.length == 0) return new bytes(0);
-        require(data.length % 4 == 0, "invalid base64 decoder input");
+        //root hash
+        while (_nodeIndex < totalSupply * 2 - 2) {
+            // find parent node
+            _nodeIndex = _nodeIndex / 2 + totalSupply;
 
-        // load the table into memory
-        bytes memory table = TABLE_DECODE;
-
-        // every 4 characters represent 3 bytes
-        uint256 decodedLen = (data.length / 4) * 3;
-
-        // add some extra buffer at the end required for the writing
-        bytes memory result = new bytes(decodedLen + 32);
-
-        assembly {
-            // padding with '='
-            let lastBytes := mload(add(data, mload(data)))
-            if eq(and(lastBytes, 0xFF), 0x3d) {
-                decodedLen := sub(decodedLen, 1)
-                if eq(and(lastBytes, 0xFFFF), 0x3d3d) {
-                    decodedLen := sub(decodedLen, 1)
-                }
-            }
-
-            // set the actual output length
-            mstore(result, decodedLen)
-
-            // prepare the lookup table
-            let tablePtr := add(table, 1)
-
-            // input ptr
-            let dataPtr := data
-            let endPtr := add(dataPtr, mload(data))
-
-            // result ptr, jump over length
-            let resultPtr := add(result, 32)
-
-            // run over the input, 4 characters at a time
-            for {
-
-            } lt(dataPtr, endPtr) {
-
-            } {
-                // read 4 characters
-                dataPtr := add(dataPtr, 4)
-                let input := mload(dataPtr)
-
-                // write 3 bytes
-                let output := add(
-                    add(
-                        shl(
-                            18,
-                            and(
-                                mload(add(tablePtr, and(shr(24, input), 0xFF))),
-                                0xFF
-                            )
-                        ),
-                        shl(
-                            12,
-                            and(
-                                mload(add(tablePtr, and(shr(16, input), 0xFF))),
-                                0xFF
-                            )
-                        )
-                    ),
-                    add(
-                        shl(
-                            6,
-                            and(
-                                mload(add(tablePtr, and(shr(8, input), 0xFF))),
-                                0xFF
-                            )
-                        ),
-                        and(mload(add(tablePtr, and(input, 0xFF))), 0xFF)
-                    )
-                )
-                mstore(resultPtr, shl(232, output))
-                resultPtr := add(resultPtr, 3)
-            }
-        }
-
-        return result;
-    }
-}
-
-// with reference to https://gist.github.com/Chmarusso/045ee79fa9a1fae55928a613044c9067
-contract myNFT is ERC721, ERC721Enumerable, Ownable {
-    mapping(string => bool) private takenNames;
-    mapping(uint256 => Attr) public attributes;
-    bytes32[] public hashes;
-
-    struct Attr {
-        string name;
-        string description;
-        uint8 anumber;
-    }
-
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal override(ERC721, ERC721Enumerable) {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
-
-    function _burn(uint256 tokenId) internal override(ERC721) {
-        super._burn(tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
-
-    constructor() ERC721("myNFT", "MNFT") {}
-
-    function uint2str(uint256 _i)
-        internal
-        pure
-        returns (string memory _uintAsString)
-    {
-        if (_i == 0) {
-            return "0";
-        }
-        uint256 j = _i;
-        uint256 len;
-        while (j != 0) {
-            len++;
-            j /= 10;
-        }
-        bytes memory bstr = new bytes(len);
-        uint256 k = len;
-        while (_i != 0) {
-            k = k - 1;
-            uint8 temp = (48 + uint8(_i - (_i / 10) * 10));
-            bytes1 b1 = bytes1(temp);
-            bstr[k] = b1;
-            _i /= 10;
-        }
-        return string(bstr);
-    }
-
-    function mint(
-        address to,
-        uint256 tokenId,
-        string memory _name,
-        string memory _description,
-        uint8 _anumber
-    ) public onlyOwner {
-        _safeMint(to, tokenId);
-        attributes[tokenId] = Attr(_name, _description, _anumber);
-
-        // prepare the information to be sent to the tree
-        bytes[4] memory info_to_tree = [
-            abi.encodePacked(msg.sender),
-            abi.encodePacked(to),
-            abi.encodePacked(tokenId),
-            abi.encodePacked(tokenURI(tokenId))
-        ];
-        // https://www.youtube.com/watch?v=yJbx07N15j0&list=PLO5VPQH6OWdULDcret0S0EYQ7YcKzrigz&index=19
-
-        for (uint256 i = 0; i < info_to_tree.length; i++) {
-            hashes.push(keccak256(abi.encodePacked(info_to_tree[i])));
-        }
-
-        uint256 n = info_to_tree.length;
-        uint256 offset = 0;
-
-        while (n > 0) {
-            for (uint256 i = 0; i < n - 1; i += 2) {
-                hashes.push(
-                    keccak256(
-                        abi.encodePacked(
-                            hashes[offset + i],
-                            hashes[offset + i + 1]
-                        )
-                    )
-                );
-            }
-            offset += n;
-            n = n / 2;
+            // update parent node by hashing two children nodes
+            _merkleTreeNodes[_nodeIndex] = keccak256(abi.encodePacked(_merkleTreeNodes[(_nodeIndex - totalSupply) * 2],
+                                                                      _merkleTreeNodes[(_nodeIndex - totalSupply) * 2 + 1]));
         }
     }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721)
-        returns (string memory)
-    {
-        string memory json = Base64.encode(
-            bytes(
-                string(
-                    abi.encodePacked(
-                        '{"name": "',
-                        attributes[tokenId].name,
-                        '",',
-                        '"attributes": [{"trait_type": "anumber", "value": ',
-                        uint2str(attributes[tokenId].anumber),
-                        "},",
-                        '{"trait_type": "description", "value": "',
-                        attributes[tokenId].description,
-                        '"}',
-                        "]}"
-                    )
-                )
-            )
-        );
-        return string(abi.encodePacked("data:application/json;base64,", json));
+    function getMerkleNode(uint256 _nodeIndex) public view returns (bytes32) {
+        return _merkleTreeNodes[_nodeIndex];
+    }
+
+    function getMerkleRootHash() public view returns (bytes32) {
+        return _merkleTreeNodes[totalSupply * 2 - 2];
     }
 }
